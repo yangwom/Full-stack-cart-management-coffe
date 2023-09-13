02b9deccda0c8367dba92c6016dbb6de2728a9cc
Yang wom vieira <yangflix3@gmail.com>
2023-09-13 11:08:26 -0300

    Delete WebApplication1 directory

--- WebApplication1/WebApplication1/Model/CartItemPrice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CartApi.Model
-{
-    public class CartItemPrice
-    {   
-        public int Id { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-    }
-}
