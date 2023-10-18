c7043de90f189f3bde2d7cb59ea1fb02b1b44448
gustavoalves23 <gustavo.alves388@gmail.com>
2023-10-18 10:06:45 -0300

    feat: implementa refetch useFetch

--- coffee-management/src/Hook/useFetch.ts
+++ coffee-management/src/Hook/useFetch.ts
@@ -4,10 +4,12 @@ import axios from "axios";
 export function useFetch<T = unknown>(url: string) {
 const [data, setData] = useState<T | null>(null)
 
+const fetchData = async () => setData(await axios.get(url))
+
 useEffect(() => {
-    axios.get(url).then(response => setData(response.data))
+    fetchData()
 }, [])
 
-return { data }
+return { data, refetch: fetchData }
 
 }
